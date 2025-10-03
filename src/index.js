b1d0b0c6c1be1c2125a7813f5f0da2a7b4214825
aLearningLad <mocmanca@gmail.com>
2025-10-03 12:51:04 +0200

    index.js setup, app instance running

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// app instance
+const app = express();
+
+// load up envs and make them usable
+dotenv.config();
+
+// port
+const PORT = process.env.DEV_PORT;
+
+// start app
+app.listen(PORT, () => {
+  console.log(`Server instance is listening on PORT: ${PORT}`);
+});
