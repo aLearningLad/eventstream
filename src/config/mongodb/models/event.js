93d6e89e175e78b793a560f6368c92be6cde3326
aLearningLad <mocmanca@gmail.com>
2025-10-04 15:56:23 +0200

    schema timestamp addition

--- src/config/mongodb/models/event.js
+++ src/config/mongodb/models/event.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
 // event metadata schema
-const eventSchema = new mongoose.Schema({
-  event_id: String,
-  s3_key: String,
-  type: String, // 'poster, billboard, thumbnail, banner, digital'
-  tags: [String],
-  uploaded_by: String, // user_id
-  created_at: Date,
-  updated_at: Date,
-  description: String, // misc info, notes for event, planning ideas
-});
+const eventSchema = new mongoose.Schema(
+  {
+    event_id: String,
+    s3_key: String,
+    type: String, // 'poster, billboard, thumbnail, banner, digital'
+    tags: [String],
+    uploaded_by: String, // user_id
+    // created_at: Date,---> mongoose adds this for me bc I set timestamps = true
+    updated_at: Date,
+    description: String, // misc info, notes for event, planning ideas
+  },
+  { timestamps: true }
+);
 
 // event model made from schema
 const AnEvent = mongoose.model("AnEvent", eventSchema);
