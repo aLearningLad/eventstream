09fd0332250efae049ee6352188d3f965be1d1c0
aLearningLad <mocmanca@gmail.com>
2025-10-04 20:02:57 +0200

    post req for metadata fleshing out

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv").config(); // load up envs and make them usable
 // app instance
 const app = express();
 
+// parse json from client
+app.use(express.json());
+
 // port
 const PORT = process.env.DEV_PORT;
 
@@ -12,20 +15,30 @@ app.listen(PORT, () => {
   console.log(`Server instance is listening on PORT: ${PORT}`);
 });
 
+// general test
+app.get("/api/v1/metadata", (req, res) => {
+  res.status(200).json({ message: "Ola, cabron!" });
+});
+
 // recieve metadata info
 app.post("/api/v1/metadata", (req, res) => {
-  const { event_id, s3_key, type, tags, uploaded_by, description } = req.body;
-
-  // handle any omissions by client
-  if (!event_id || !s3_key || !type || !tags || !uploaded_by || !description) {
-    console.log("Request body is incomplete. Please try again");
-    res.status(400).json({
-      message:
-        "Request body is incomplete. Please try again and ensure all fields are complete",
-    });
-  }
-
-  // if no client omissions, send data to mongodb (later this becomes: from this route's controller, send as kafka producer TO kafka consumer)
   try {
-  } catch (error) {}
+    // destructure, pull out all values to be saved
+    const { event_id, s3_key, type, tags, uploaded_by, description } = req.body; // --> will throw error if any value is missing from req.body
+
+    // alert client if all metadata fields are present
+    if (event_id && s3_key && type && tags && uploaded_by && description) {
+      res.status(200).json({
+        message: "All fields are complete! Commencing metdata upload!",
+      });
+    }
+
+    // if no client omissions, send data to mongodb (later this becomes: from this route's controller, send as kafka producer TO kafka consumer)
+  } catch (error) {
+    res.status(400).json({ message: `An error occured: ${error} ` });
+    console.error(
+      "An error occured while trying to send metadata to MongoDB: ",
+      error
+    );
+  }
 });
