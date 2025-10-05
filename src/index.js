a5b8dca6a596dfd547238f6c514c193d27ecadcb
aLearningLad <mocmanca@gmail.com>
2025-10-05 12:16:02 +0200

    metadata upload route refactored

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv").config(); // load up envs and make them usable
 const connectToDb = require("./config/mongodb/database/db");
 const EventModel = require("./config/mongodb/models/event");
+const {
+  uploadMetadataRoute,
+} = require("./routes/metadata_routes/upload.routes");
 
 // app instance
 const app = express();
@@ -27,39 +30,4 @@ app.get("/api/v1/metadata", (req, res) => {
 });
 
 // recieve metadata info
-app.post("/api/v1/metadata", (req, res) => {
-  try {
-    // destructure, pull out all values to be saved
-    const { event_id, s3_key, type, tags, uploaded_by, description } = req.body; // --> will throw error if any value is missing from req.body
-
-    // alert client if all metadata fields are present
-    if (event_id && s3_key && type && tags && uploaded_by && description) {
-      res.status(200).json({
-        message: "All fields are complete! Commencing metdata upload!",
-      });
-    }
-
-    // if no client omissions, send data to mongodb (later this becomes: from this route's controller, send as kafka producer TO kafka consumer)
-    const newMetadata = new EventModel({
-      description,
-      event_id,
-      s3_key,
-      type,
-      tags,
-      uploaded_by,
-    });
-
-    newMetadata
-      .save()
-      .then(() => console.log("New metadata entry saved"))
-      .catch((err) =>
-        console.error("Unable to save new metadata entry: ", err)
-      );
-  } catch (error) {
-    res.status(400).json({ message: `An error occured: ${error} ` });
-    console.error(
-      "An error occured while trying to send metadata to MongoDB: ",
-      error
-    );
-  }
-});
+app.use(uploadMetadataRoute);
---
+++ src/routes/metadata_routes/upload.routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const EventModel = require("./config/mongodb/models/event");
+
+const router = express.Router();
+
+router.post("/api/v1/metadata", async (req, res) => {
+  // destructure, pull out all values to be saved
+  const { event_id, s3_key, type, tags, uploaded_by, description } = req.body;
+
+  // alert client if any metadata fields are missing
+  if (!event_id || !s3_key || !type || !tags || !uploaded_by || !description) {
+    console.error("Metadata fields are incomplete");
+    return res.status(400).json({ message: "Metadata fields are incomplete" });
+  }
+
+  try {
+    // if no client omissions, send data to mongodb (later this becomes: from this route's controller, send as kafka producer TO kafka consumer)
+    const newMetadata = new EventModel({
+      description,
+      event_id,
+      s3_key,
+      type,
+      tags,
+      uploaded_by,
+    });
+
+    await newMetadata.save();
+    return res.status(201).json({ success: "New metadata entry saved" });
+  } catch (error) {
+    console.error(
+      "An error occurred while trying to send metadata to MongoDB: ",
+      error
+    );
+    return res
+      .status(500)
+      .json({ message: `An error occured while saving metadata` });
+  }
+});
+
+module.exports = { uploadMetadataRoute: router };
