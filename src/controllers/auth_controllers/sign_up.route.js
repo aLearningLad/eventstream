97b945f212b1a7b3a5a513dbe193c1fb4b9211b2
aLearningLad <mocmanca@gmail.com>
2025-10-06 18:36:43 +0200

    sign up route refactoring

---
+++ src/controllers/auth_controllers/sign_up.route.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcrypt");
+const db_client = require("../../config/postgresql/client");
+const db = db_client;
+const saltRounds = 10;
+
+const signUpController = async (req, res) => {
+  //  no req.body
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      message:
+        "Request does not include a payload. No request body is provided",
+    });
+  }
+
+  const { full_name, email, role, password } = req.body;
+
+  // confirm payload from client
+  if (!full_name || !password) {
+    return res.status(401).json({ message: "User credentials are missing" });
+  }
+
+  // magic sauce to protect password
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      //   insert into postgresql
+      const { error: user_reg_error } = await db.from("users").insert({
+        full_name,
+        password,
+        email,
+        role,
+      });
+
+      if (user_reg_error) {
+        return res
+          .status(500)
+          .json({ message: "Unable to save new user: ", user_reg_error });
+      }
+
+      return res.status(201).json({ message: "New user registered" });
+    });
+  });
+};
+
+module.exports = signUpController;
--- src/routes/auth_routes/sign_up.route.js
+++ src/routes/auth_routes/sign_up.route.js
@@ -1,49 +1,7 @@
-const passport = require("passport");
-const localStrategy = require("passport-local");
 const express = require("express");
-const db_client = require("../../config/postgresql/client");
-const bcrypt = require("bcrypt");
-
+const signUpController = require("../../controllers/auth_controllers/sign_up.route");
 const router = express.Router();
-const db = db_client;
-const saltRounds = 10;
 
-router.post("/api/v1/sign-up", async (req, res) => {
-  //  no req.body
-  if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).json({
-      message:
-        "Request does not include a payload. No request body is provided",
-    });
-  }
-
-  const { full_name, email, role, password } = req.body;
-
-  // confirm payload from client
-  if (!full_name || !password) {
-    return res.status(401).json({ message: "User credentials are missing" });
-  }
-
-  // magic sauce to protect password
-  bcrypt.genSalt(saltRounds, function (err, salt) {
-    bcrypt.hash(password, salt, async function (err, hash) {
-      //   insert into postgresql
-      const { error: user_reg_error } = await db.from("users").insert({
-        full_name,
-        password,
-        email,
-        role,
-      });
-
-      if (user_reg_error) {
-        return res
-          .status(500)
-          .json({ message: "Unable to save new user: ", user_reg_error });
-      }
-
-      return res.status(201).json({ message: "New user registered" });
-    });
-  });
-});
+router.post("/api/v1/sign-up", signUpController);
 
 module.exports = { signUpRoute: router };
