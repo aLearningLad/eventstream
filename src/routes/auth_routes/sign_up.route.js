5215e065d9d75edd678475a21fb9a955d2e1000b
aLearningLad <mocmanca@gmail.com>
2025-10-06 15:43:54 +0200

    bcrypt wireframing

--- src/routes/auth_routes/sign_up.route.js
+++ src/routes/auth_routes/sign_up.route.js
@@ -1,17 +1,29 @@
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const express = require("express");
+const db_client = require("../../config/postgresql/client");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
+const db_client = db_client();
+const saltRounds = 10;
 
 router.post("/api/v1/sign-up", (req, res) => {
   const { username, password } = req.body;
 
+  // confirm payload from client
   if (!username || !password) {
     return res.status(401).json({ message: "User credentials are missing" });
   }
 
-  passport.use(new localStrategy(function verify(username, password, cb) {}));
+  // magic sauce to protect password
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      console.log(
+        `Password text is ${password}, and when hashed it is: ${hash}`
+      );
+    });
+  });
 });
 
 module.exports = { signUpRoute: router };
