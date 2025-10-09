c627676e6af747112757eea07f2aaa2279a4a862
aLearningLad <mocmanca@gmail.com>
2025-10-10 00:45:11 +0200

    kafka refactoring for project event

---
+++ src/services/project/event_upload/producer.js
@@ -0,0 +1,52 @@
+const kafka = require("../../../config/kafka/kafka");
+
+const projectEventToKafka = async ({
+  organizer_id,
+  title,
+  description,
+  location,
+  start_time,
+  end_time,
+  price,
+  capacity,
+  date,
+}) => {
+  // handle ommissions
+  if (
+    !organizer_id ||
+    !title ||
+    !description ||
+    !location ||
+    !start_time ||
+    !end_time ||
+    !price ||
+    !capacity ||
+    !date
+  ) {
+    return 400;
+  }
+
+  // no issues
+  const producer = kafka.producer();
+
+  await producer.connect();
+
+  await producer.send({
+    topic: "upload-project-event",
+    messages: [
+      {
+        value: JSON.stringify({
+          organizer_id,
+          title,
+          description,
+          location,
+          start_time,
+          end_time,
+          price,
+          capacity,
+          date,
+        }),
+      },
+    ],
+  });
+};
