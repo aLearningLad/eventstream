b6c7e42da8298807fdd9a2dca4c6ed32dc650a38
aLearningLad <mocmanca@gmail.com>
2025-10-11 02:24:28 +0200

    attendee sign up route complete

--- src/routes/attendee_routes/sign_up.js
+++ src/routes/attendee_routes/sign_up.js
@@ -1,16 +1,40 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const db_client = require("../../config/postgresql/client");
+const db = db_client;
 
 router.post("/api/v1/attendee/sign-up", (req, res) => {
   if (!req.body) {
-    return res.status(400).json({ message: "Missing fields" });
+    return res.status(401).json({ message: "Missing fields" });
   }
 
   const role = "attendee";
   const { full_name, email, password } = req.body;
 
-  // hash password
+  // ommissions
+  if (!full_name || !email || !password) {
+    return res.status(401).json({ message: "User credentials are incomplete" });
+  }
 
-  return res.status(200).json({ message: "This route woooorks" });
+  // hash password
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      // insert user into postgresql
+      const { error: new_user_error } = await db.from("users").insert({
+        full_name,
+        password: hash,
+        email,
+        role,
+      });
+
+      if (new_user_error) {
+        return res.status(500).json({ error: "Unable to sign user up" });
+      }
+    });
+  });
+
+  return res.status(200).json({ success: "User successfully signed up" });
 });
 
 module.exports = { attendeeSignUpRoute: router };
