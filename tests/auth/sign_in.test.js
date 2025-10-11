a0242a75f4f8ec2fb71a9bf8c88ee4f772fefbe2
aLearningLad <mocmanca@gmail.com>
2025-10-11 22:16:01 +0200

    refactoring for tests

--- tests/auth/sign_in.test.js
+++ tests/auth/sign_in.test.js
@@ -1,1 +1,16 @@
-const app = require("../../src/index");
+jest.mock("../../src/consumers/metadata/upload");
+jest.mock("../../src/consumers/event/upload");
+jest.mock("../../src/consumers/project/project_event/upload_event");
+const app = require("../../src/app");
+const request = require("supertest");
+
+describe("Auth endpoint", () => {
+  it("User should be able to sign in and recieve confirmation", async () => {
+    const res = await request(app).post("/api/v1/sign-in").send({
+      email: "bombaclaat@gmail.com",
+      password: "test12345",
+    });
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
