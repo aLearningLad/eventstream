b26767c9c19ba7fd1dc72f37dcef60ee3d98ea87
aLearningLad <mocmanca@gmail.com>
2025-10-12 00:38:49 +0200

    upload test refactoring to mock cookies

--- tests/organizers/upload_event.test.js
+++ tests/organizers/upload_event.test.js
@@ -3,21 +3,40 @@ jest.mock("../../src/consumers/event/upload");
 jest.mock("../../src/consumers/project/project_event/upload_event");
 const app = require("../../src/app");
 const request = require("supertest");
+const mongoose = require("mongoose");
 
-describe("Auth endpoint", () => {
-  it("User should be able to sign in and recieve confirmation", async () => {
-    const res = await request(app).post("/api/v1/event").send({
-      organizer_id: 3,
-      title: "Coke festival",
-      description: "Same as fanta, but for coke heads this time!",
-      location: "Paarl, ZA",
-      start_time: "2020-06-22 19:10:25-07",
-      end_time: " 2020-06-22 19:10:25-07",
-      price: 7400,
-      capacity: 4000,
-      date: "2026-06-22",
-    });
+// so let user sign in first, otherwise my response is a 401 due to auth middleware bruuuuh
+let cookies;
+beforeAll(async () => {
+  const res = (await request(app).post("/api/v1/sign-in")).setEncoding({
+    email: "test1@gmail.com",
+    password: "bruuuv12&87",
+  });
 
-    expect(res.statusCode).toEqual(200);
+  cookies = res.headers["set-cookie"];
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe("Organizer uploads core event details", () => {
+  it("User should be able to send event data and recieve confirmation of upload", async () => {
+    const res = await request(app)
+      .post("/api/v1/event")
+      .set("Cookie", cookies)
+      .send({
+        organizer_id: 3,
+        title: "Coke festival",
+        description: "Same as fanta, but for coke heads this time!",
+        location: "Paarl, ZA",
+        start_time: "2020-06-22 19:10:25-07",
+        end_time: " 2020-06-22 19:10:25-07",
+        price: 7400,
+        capacity: 4000,
+        date: "2026-06-22",
+      });
+
+    expect(res.statusCode).toEqual(201);
   });
 });
