fb516577d9ba005a026bd91b3dcf0d90b979750b
aLearningLad <mocmanca@gmail.com>
2025-10-11 20:59:48 +0200

    get project route complete

--- src/routes/project_routes/get_project.route.js
+++ src/routes/project_routes/get_project.route.js
@@ -3,21 +3,23 @@ const sqldb = require("../../config/postgresql/client");
 const mongoose = require("mongoose");
 const aws = require("../../config/aws/s3_client");
 const metadata_model = require("../../config/mongodb/models/event");
+const { GetObjectCommand } = require("@aws-sdk/client-s3");
 
 const postgresql = sqldb;
+const s3_client = aws;
 
 router.get("/api/v1/event/:event_id", async (req, res) => {
   if (!req.body) {
     return res.status(400).json({ message: "No request payload found" });
   }
   const { event_id } = req.params;
-  const { organizer_id } = req.body;
+  const { organizer_id, company_name } = req.body;
 
   if (!event_id) {
     return res.status(400).json({ message: "No event id found in params" });
   }
 
-  if (!organizer_id) {
+  if (!organizer_id || !company_name) {
     console.error("Missing fields");
     return res.status(400).json({ message: "Missing fields" });
   }
@@ -48,12 +50,23 @@ router.get("/api/v1/event/:event_id", async (req, res) => {
     const doc = await metadata_model.findById(mongo_id);
 
     // 3. fetch aws s3 media
+    const bucket_name = process.env.AWS_S3_BUCKET_NAME;
+    const s3_key = await combined_sql_data[0].event_metadata_links[0].s3_key;
+    const result = await s3_client.send(
+      new GetObjectCommand({
+        Bucket: bucket_name,
+        Key: s3_key,
+      })
+    );
+
+    const raw_data = await result.Body.transformToString();
+    const base64image = Buffer.from(raw_data).toString("base64");
 
     return res.status(200).json({
       success: "Data found",
       core_event_data: combined_sql_data,
       metadata: doc,
-      media_files: "coming soon",
+      media_files: `data:image/jpeg;base64,${base64image}`,
     });
   } catch (error) {
     console.error("Unable to fetch project data: ", error);
