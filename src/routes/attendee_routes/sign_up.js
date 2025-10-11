1ea0d8199cdcce5626cd35ce5359f50a93fbbb42
aLearningLad <mocmanca@gmail.com>
2025-10-11 02:11:41 +0200

    sign up flow logic fleshing out

--- src/routes/attendee_routes/sign_up.js
+++ src/routes/attendee_routes/sign_up.js
@@ -5,7 +5,10 @@ router.post("/api/v1/attendee/sign-up", (req, res) => {
     return res.status(400).json({ message: "Missing fields" });
   }
 
-  const {} = req.body;
+  const role = "attendee";
+  const { full_name, email, password } = req.body;
+
+  // hash password
 
   return res.status(200).json({ message: "This route woooorks" });
 });
