62addf63ab2038df663150fa47e8790e1e2aa776
aLearningLad <mocmanca@gmail.com>
2025-10-11 21:30:56 +0200

    controller refactoring

---
+++ src/controllers/attendee_controllers/purchase_controller.js
@@ -0,0 +1,39 @@
+const db = sql;
+const sql = require("../../config/postgresql/client");
+
+const purchaseController = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ message: "No payload sent with request" });
+  }
+
+  const { event_id } = req.params;
+  const { user_id } = req.body;
+
+  if (!event_id || !user_id) {
+    console.error("Event and/or User IDs ommitted");
+    return res
+      .status(400)
+      .json({ message: "IDs are required for ticket purchase" });
+  }
+
+  const { data: ticket_data, error: ticket_data_error } = await db
+    .from("tickets")
+    .insert({
+      user_id,
+      event_id,
+    })
+    .select();
+
+  if (ticket_data_error) {
+    console.error(ticket_data_error);
+    res.status(500).json({ message: "Unable to purchase tickets" });
+  }
+
+  const ticket_id = await ticket_data[0].ticket_id;
+
+  res
+    .status(200)
+    .json({ success: "Ticket purchased!", ticket_number: ticket_id });
+};
+
+module.exports = purchaseController;
--- src/routes/attendee_routes/purchase_ticket.js
+++ src/routes/attendee_routes/purchase_ticket.js
@@ -1,47 +1,13 @@
 const router = require("express").Router();
-const sql = require("../../config/postgresql/client");
+const purchaseController = require("../../controllers/attendee_controllers/purchase_controller");
 const app_limiter = require("../../middleware/app_limiter");
 const ensureAuth = require("../../middleware/is_authenticated");
-const db = sql;
 
 router.post(
   "/api/v1/attendee/buy-ticket/:event_id",
   ensureAuth,
   app_limiter,
-  async (req, res) => {
-    if (!req.body) {
-      return res.status(400).json({ message: "No payload sent with request" });
-    }
-
-    const { event_id } = req.params;
-    const { user_id } = req.body;
-
-    if (!event_id || !user_id) {
-      console.error("Event and/or User IDs ommitted");
-      return res
-        .status(400)
-        .json({ message: "IDs are required for ticket purchase" });
-    }
-
-    const { data: ticket_data, error: ticket_data_error } = await db
-      .from("tickets")
-      .insert({
-        user_id,
-        event_id,
-      })
-      .select();
-
-    if (ticket_data_error) {
-      console.error(ticket_data_error);
-      res.status(500).json({ message: "Unable to purchase tickets" });
-    }
-
-    const ticket_id = await ticket_data[0].ticket_id;
-
-    res
-      .status(200)
-      .json({ success: "Ticket purchased!", ticket_number: ticket_id });
-  }
+  purchaseController
 );
 
 module.exports = { attendeePurchaseTicketRoute: router };
