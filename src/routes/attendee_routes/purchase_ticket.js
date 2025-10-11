f58207a696af0b2c80d61ae0214e838f2597da79
aLearningLad <mocmanca@gmail.com>
2025-10-11 21:12:01 +0200

    ticket route fleshing out

--- src/routes/attendee_routes/purchase_ticket.js
+++ src/routes/attendee_routes/purchase_ticket.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 
 router.post("/api/v1/attendee/buy-ticket", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ message: "No payload sent with request" });
+  }
+
   res.status(200).json({ success: "Ticket purchased!" });
 });
 
