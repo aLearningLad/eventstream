6c4e2a59366e06b3ea6e777f7f5e9c46d9ea3a92
aLearningLad <mocmanca@gmail.com>
2025-10-11 18:06:45 +0200

    fetch project producer outline

--- src/routes/project_routes/get_project.route.js
+++ src/routes/project_routes/get_project.route.js
@@ -1,15 +1,30 @@
+const fetchProjectProducer = require("../../services/project/get_event/producer");
+
 const router = require("express").Router();
 
 router.get("/api/v1/event/:event_id", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ message: "No request payload found" });
+  }
   const { event_id } = req.params;
+  const { organizer_id } = req.body;
 
   if (!event_id) {
     return res.status(400).json({ message: "No event id found in params" });
   }
 
-  // pass event id to producer
+  if (!organizer_id) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
 
-  res.status(200).json({ success: `Event ID from params is ${event_id}` });
+  // pass event id to producer
+  const result = await fetchProjectProducer({ event_id, organizer_id });
+
+  if (result === 400) {
+    return res.status(400).json({ message: "Unable to fetch event data" });
+  }
+
+  res.status(200).json({ success: "Event data retrieved" });
 });
 
 module.exports = { retrieveProjectRoute: router };
--- src/services/project/get_event/producer.js
+++ src/services/project/get_event/producer.js
@@ -1,5 +1,27 @@
 const kafka = require("../../../config/kafka/kafka");
 
-const fetchProjectProducer = async () => {};
+const fetchProjectProducer = async ({ event_id, organizer_id }) => {
+  if (!event_id || !organizer_id) {
+    return 400;
+  }
+
+  const producer = kafka.producer();
+
+  await producer.connect();
+
+  await producer.send({
+    topic: "fetch-project",
+    messages: [
+      {
+        value: JSON.stringify({
+          event_id,
+          organizer_id,
+        }),
+      },
+    ],
+  });
+
+  return 200;
+};
 
 module.exports = fetchProjectProducer;
