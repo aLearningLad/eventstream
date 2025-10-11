885d5c073b4d8d340a4bc918ffad2c32c61ba450
aLearningLad <mocmanca@gmail.com>
2025-10-11 14:10:43 +0200

    waitlist route fleshing out

--- src/routes/attendee_routes/join_waitlist.js
+++ src/routes/attendee_routes/join_waitlist.js
@@ -1,7 +1,39 @@
+const ensureAuth = require("../../middleware/is_authenticated");
+const db_client = require("../../config/postgresql/client");
+const db = db_client;
+
 const router = require("express").Router();
 
-router.post("/api/v1/attendee/join-waiting-list", async (req, res) => {
-  return res.status(200).json({ success: "waiting list route works!" });
-});
+router.post(
+  "/api/v1/attendee/join-waiting-list",
+  ensureAuth,
+  async (req, res) => {
+    if (!req.body) {
+      return res.status(400).json({ message: "No request payload sent" });
+    }
+
+    const { event_id, user_id } = req.body;
+
+    if (!event_id || !user_id) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+
+    const { error: waiting_list_error } = await db.from("waitlist").insert({
+      event_id,
+      user_id,
+    });
+
+    if (waiting_list_error) {
+      console.error(
+        "Unable to subscribe to waiting list: ",
+        waiting_list_error
+      );
+      return res
+        .status(500)
+        .json({ message: "Unable to subscribe to waiting list" });
+    }
+    return res.status(200).json({ success: "waiting list route works!" });
+  }
+);
 
 module.exports = { joinWaitingListRoute: router };
